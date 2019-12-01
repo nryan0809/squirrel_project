3ce789c1e29395e0406a6c8d38bee847d0c0a136
zihao ying <zy2389@columbia.edu>
2019-12-01 21:37:02 +0000

    dd

--- vwsquirrel/views.py
+++ vwsquirrel/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import squirrel_model
+from django.http import HttpResponse
+
+def index(request,x):
+    return HttpResponse(squirrel_model.objects.all())
 
 # Create your views here.
