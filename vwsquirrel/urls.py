31cb1e3c8459aeef99668fb43d912baa4932489a
zihao ying <zy2389@columbia.edu>
2019-12-01 22:02:38 +0000

    dd

--- vwsquirrel/urls.py
+++ vwsquirrel/urls.py
@@ -1,16 +1,6 @@
 from django.urls import path
-<<<<<<< HEAD
-
-from . import views
-
-urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index')
-    ]
-=======
 from . import views
 app_name = 'vwsquirrel'
 urlpatterns = [
         path('', views.index, name='index'),
         ]
->>>>>>> 8226e84c9c6fad8e31d65a22447fa3d6d936159e
--- vwsquirrel/views.py
+++ vwsquirrel/views.py
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-from .models import squirrel_model
-from django.http import HttpResponse
-
-def index(request,x):
-    return HttpResponse(squirrel_model.objects.all())
-
-=======
 from django.shortcuts import render,get_object_or_404
 from django.apps import apps
->>>>>>> 8226e84c9c6fad8e31d65a22447fa3d6d936159e
 # Create your views here.
 from django.http import HttpResponseRedirect
 from django.urls import reverse
